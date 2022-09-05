2dae0443ac7f13d03a98c2b0e5fd356472bb3e9e
Jaqueline <jaquelinesv90@gmail.com>
2022-09-05 08:00:59 -0700

    querying Singleton Resource with GET and PathVariable

--- src/main/java/com/algaworks/algafood/api/controller/KitchenController.java
+++ src/main/java/com/algaworks/algafood/api/controller/KitchenController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +23,10 @@ public class KitchenController {
 		return repository.all();
 	}
 	
-	
+	@GetMapping("/{kitchenId}")
+	public Kitchen findById(@PathVariable("kitchenId") Long id) {
+		return repository.findById(id);
+	}
 	
 	
 }
