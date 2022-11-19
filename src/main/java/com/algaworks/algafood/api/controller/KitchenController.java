858ce2e5d2f14cc015646c83586b458232a04e62
jaquelinesv90 <jaquelinesv90@gmail.com>
2022-11-19 14:26:10 -0800

    added first part of delete implementation

--- src/main/java/com/algaworks/algafood/api/controller/KitchenController.java
+++ src/main/java/com/algaworks/algafood/api/controller/KitchenController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,6 +61,12 @@ public class KitchenController {
 			return ResponseEntity.ok(currentKitchen);
 		}
 		return ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{kitchenId}")
+	public ResponseEntity<Kitchen> delete(@PathVariable Long kitchenId){
+		Kitchen kitchen = repository.findById(kitchenId);
+		
 	}
 	
 }
