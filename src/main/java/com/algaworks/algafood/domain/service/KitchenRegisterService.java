4a7d126ba842577d4626072a54b14fa9198a8321
jaquelinesv90 <jaquelinesv90@gmail.com>
2022-11-21 13:49:38 -0800

    first part of domain services implementation

---
+++ src/main/java/com/algaworks/algafood/domain/service/KitchenRegisterService.java
@@ -0,0 +1,19 @@
+package com.algaworks.algafood.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.algaworks.algafood.domain.model.Kitchen;
+import com.algaworks.algafood.domain.repository.KitchenRepository;
+
+@Service
+public class KitchenRegisterService {
+	
+	@Autowired 
+	private KitchenRepository repository;
+	
+	public Kitchen save(Kitchen kitchen) {
+		return repository.save(kitchen);
+	}
+
+}
