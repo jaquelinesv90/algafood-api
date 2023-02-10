c39e9460076b110d025705a949263bb311f8ecf6
jaquelinesv90 <jaquelinesv90@gmail.com>
2023-02-10 14:01:57 -0800

    added new method- merge

--- src/main/java/com/algaworks/algafood/api/controller/RestaurantController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestaurantController.java
@@ -1,12 +1,16 @@
 package com.algaworks.algafood.api.controller;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -77,5 +81,32 @@ public class RestaurantController {
 			return ResponseEntity.badRequest()
 					.body(e.getMessage());
 		}
+	}
+	
+	@PatchMapping("/{restaurantId}")
+	public ResponseEntity<?> partialUpdate(@PathVariable Long restaurantId, 
+			@RequestBody Map<String, Object> fields){
+		Restaurant currentRestaurant = repository.findById(restaurantId);
+		
+		if(currentRestaurant == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		merge(fields, currentRestaurant);
+		
+		return update(restaurantId, currentRestaurant);
+	}
+
+	// reflectionUtils busca pra mim um campo(field) chamado nome, da classe Restaurant 
+	// esse campo field vai representar o nome
+	// isso é feito para atribuir o valor do field no objeto Restaurantdestino o valor da propriedade 
+	private void merge(Map<String, Object> originField, Restaurant destinyRestaurant) {
+		originField.forEach((nomePropriedade, valorPropriedade) -> {
+			Field field = ReflectionUtils.findField(Restaurant.class,nomePropriedade);
+			
+			System.out.println(nomePropriedade + "=" + valorPropriedade);
+			
+			ReflectionUtils.setField(field, destinyRestaurant, valorPropriedade);
+		});
 	}
 }
